c543489d3936bd8e01d947dd5752d8afb839c590
DontDDoS <git@dontddos.me>
2026-08-13 03:38:23 +0200

    Updates

---
+++ server/middleware-build-manifest.js
@@ -0,0 +1,23 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/-wThoSUtxRDQTnzUD4q_q/_buildManifest.js",
+    "static/-wThoSUtxRDQTnzUD4q_q/_ssgManifest.js",
+    "static/-wThoSUtxRDQTnzUD4q_q/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/12aruqaur5huj.js",
+    "static/chunks/0xpvskz-ar_4u.js",
+    "static/chunks/4561u0v7ysn3r.js",
+    "static/chunks/turbopack-006ya0efe5sjk.js"
+  ],
+  "rootMainFilesTree": {},
+  "pagesChunkGroupBootstrapParams": {},
+  "chunkLoadingGlobal": "TURBOPACK"
+};
---
+++ server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7ORQfSWVkCW7N/faGcgPS0m7sebavny7NWIGxvBq1OU=\"\n}"
